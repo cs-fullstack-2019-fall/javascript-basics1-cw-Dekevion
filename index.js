cd69e2b9217186f82a9d4c276508748ae014f2d0
Dekevion Wade <Dekevionwade@yahoo.com>
2019-08-06 15:54:38 -0500

    I answered the questions

---
+++ index.js
@@ -0,0 +1,15 @@
+// problem 1
+alert("Hello World");
+
+var nameVariable = "Dekevion";
+//problem 2
+alert("Hello " + nameVariable);
+
+//problem 3
+var dogAge = 1;
+
+alert("Your dog is: " + dogAge * 7);
+
+// problem 4
+
+alert("1 + 1 = " + 2);
